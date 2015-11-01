3a1e9b729f5e0679fbfb6ed747a0ec58a37e882e
Franz Brummer <linuxfranz@users.noreply.github.com>
2015-11-01 19:21:11 +0100

    WallFollower

--- chapter07.js
+++ chapter07.js
@@ -99,6 +99,29 @@ BouncingCritter.prototype.act = function(view) {
   return {type: "move", direction: this.direction};
 };
 
+/*
+ Critter that walks along wall clock wise
+*/
+
+function dirPlus(dir, n) {
+  var index = directionNames.indexOf(dir);
+  return directionNames[(index + n + 8) % 8];
+}
+
+function WallFollower() {
+  this.dir = "s";
+}
+
+WallFollower.prototype.act = function(view) {
+  var start = this.dir;
+  if (view.look(dirPlus(this.dir, -3)) != " ")
+    start = this.dir = dirPlus(this.dir, -2);
+  while (view.look(this.dir) != " ") {
+    this.dir = dirPlus(this.dir, 1);
+    if (this.dir == start) break;
+  }
+  return {type: "move", direction: this.dir};
+};
 
 /*
   wall has no act method
