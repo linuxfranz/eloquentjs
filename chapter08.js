a1f9b07a2e132326b0a083002e710b147020294d
Franz Brummer <linuxfranz@users.noreply.github.com>
2016-01-10 12:07:08 +0100

    Locked Box Exercise

--- chapter08.js
+++ chapter08.js
@@ -24,3 +24,44 @@ function reliableMultiply(a, b) {
 
 console.log(reliableMultiply(8, 8));
 // → 64
+
+
+// The Locked Box
+
+var box = {
+  locked: true,
+  unlock: function() { this.locked = false; },
+  lock: function() { this.locked = true;  },
+  _content: [],
+  get content() {
+    if (this.locked) throw new Error("Locked!");
+    return this._content;
+  }
+};
+
+function withBoxUnlocked(body) {
+  var locked = box.locked;
+  box.unlock();
+  try {
+    body.call();
+  }
+  finally {
+    if (locked) {
+      box.lock();
+    }
+  }
+}
+
+withBoxUnlocked(function() {
+  box.content.push("gold piece");
+});
+
+try {
+  withBoxUnlocked(function() {
+    throw new Error("Pirates on the horizon! Abort!");
+  });
+} catch (e) {
+  console.log("Error raised:", e);
+}
+console.log(box.locked);
+// → true
